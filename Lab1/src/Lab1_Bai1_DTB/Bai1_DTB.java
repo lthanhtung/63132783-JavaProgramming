89754630194f587810115d982790b6ea78161854
Lê Thanh Tùng <130471358+lthanhtung@users.noreply.github.com>
2025-02-11 21:24:26 +0700

    Lab1_Bai1_DTB

---
+++ Lab1/src/Lab1_Bai1_DTB/Bai1_DTB.java
@@ -0,0 +1,17 @@
+
+package Lab1_Bai1_DTB;
+
+import java.util.Scanner;
+
+public class Bai1_DTB {
+
+	public static void main(String[] args) {
+		Scanner nhap = new Scanner(System.in);
+		String HotenSV = nhap.nextLine();
+		Double DTB = nhap.nextDouble();
+		System.out.println("Họ tên sinh viên:"+HotenSV);
+		System.out.println("Điểm trung bình:"+DTB);
+		
+	}
+
+}
